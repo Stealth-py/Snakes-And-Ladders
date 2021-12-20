f0e7a84c3f7f343f200c76fea17a2d8766015baa
Neemesh Yadav <52747708+Stealth-py@users.noreply.github.com>
2021-12-21 02:49:39 +0530

    Delete Snake&Ladders directory

--- Snake&Ladders/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.ap_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.example.ap_project to javafx.fxml;
-    exports com.example.ap_project;
-}
