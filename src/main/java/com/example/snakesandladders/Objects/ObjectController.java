433f1a3e7dd22d5193115564933f01058fac5733
Stealth-py <generalom1234@gmail.com>
2021-12-22 15:42:31 +0530

    added id to dice image

--- src/main/java/com/example/snakesandladders/Objects/ObjectController.java
+++ src/main/java/com/example/snakesandladders/Objects/ObjectController.java
@@ -2,8 +2,6 @@ package com.example.snakesandladders.Objects;
 
 import com.example.snakesandladders.Board.gameBoard;
 
-import java.util.Random;
-
 public class ObjectController {
     private gameBoard gameboard;
 
